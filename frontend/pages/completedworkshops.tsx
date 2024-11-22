40a41a5430a605cb361d130101ad1e4dff3ae7bd
I2-S3 <israelsolomont@gmail.com>
2024-11-21 23:43:32 -0500

    Added drop down menu
    I don't know what I did, it works though.

--- frontend/pages/completedworkshops.tsx
+++ frontend/pages/completedworkshops.tsx
@@ -1,32 +1,43 @@
 // pages/completedworkshops.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/completedworkshops.css';
 import Image from 'next/image';
 import bioTrainLogo from '../public/biotrainlogo.png';
 
 const CompletedWorkshopsPage = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  
+  const handleYesClick = () => {
+    setShowDropdown(true); // Show the dropdown
+  };
+
+  
+  const handleNoClick = () => {
+    setShowDropdown(false); // Hide the dropdown
+  };
+
   return (
     <div className="container">
       <Image src={bioTrainLogo} alt="Bio train Logo" width={339} height={141} />
       <h1 className="header">Have you completed any workshops previously?</h1>
-      {/*<div className="box-container">
-        <div className="box1">
-          <h2>Soft Skills Workshops</h2>
-          <p className="p1">Soft skills workshops help you improve competencies required to work effectively in teams and adapt to corporate culture.</p>
-        </div>
-
-        <div className="box2">
-          <h2>Technical Skills Workshops</h2>
-          <p className="p1">These workshops focus on the technical skills needed to excel in the biotech industry, including hands-on tasks and advanced techniques.</p>
-        </div>
-      </div>*/}
 
       <div className="button-container">
-        <button className="btn">Yes</button>
-        <button className="btn">No</button>
+        <button className="btn" onClick={handleYesClick}>Yes</button>
+        <button className="btn" onClick={handleNoClick}>No</button>
       </div>
+
+      {showDropdown && ( 
+        
+          <div className="checkbox-list">
+            <label> <input type="checkbox" name="workshops" value="option1" /> Bioprocessing 1</label>
+            <label> <input type="checkbox" name="workshops" value="option2" />Bioprocessing 2</label>
+            <label><input type="checkbox" name="workshops" value="option3" />Bioprocessing 3</label>
+          </div>
+      )}
     </div>
   );
 };
 
 export default CompletedWorkshopsPage;
+
