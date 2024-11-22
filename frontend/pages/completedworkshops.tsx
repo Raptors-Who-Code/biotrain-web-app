63e512d68fe8cb6666ff9c31989ee0c3fbe8dac8
I2-S3 <israelsolomont@gmail.com>
2024-11-21 20:20:49 -0500

    Update completedworkshops.tsx
    changed name

---
+++ frontend/pages/completedworkshops.tsx
@@ -0,0 +1,32 @@
+// pages/completedworkshops.tsx
+import React from 'react';
+import '../styles/goalsPage.css';
+import Image from 'next/image';
+import bioTrainLogo from '../public/biotrainlogo.png';
+
+const CompletedWorkshopsPage = () => {
+  return (
+    <div className="container">
+      <Image src={bioTrainLogo} alt="Bio train Logo" width={339} height={141} />
+      <h1 className="header">Completed Workshops</h1>
+      <div className="box-container">
+        <div className="box1">
+          <h2>Soft Skills Workshops</h2>
+          <p className="p1">Soft skills workshops help you improve competencies required to work effectively in teams and adapt to corporate culture.</p>
+        </div>
+
+        <div className="box2">
+          <h2>Technical Skills Workshops</h2>
+          <p className="p1">These workshops focus on the technical skills needed to excel in the biotech industry, including hands-on tasks and advanced techniques.</p>
+        </div>
+      </div>
+
+      <div className="button-container">
+        <button className="btn">View Soft Skills Workshops</button>
+        <button className="btn">View Technical Skills Workshops</button>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedWorkshopsPage;
