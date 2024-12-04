dbcb73f16e9c0542e735b157f50792627f33726b
I2-S3 <israelsolomont@gmail.com>
2024-12-04 16:44:24 -0500

    Update completedworkshops.tsx

--- frontend/pages/completedworkshops.tsx
+++ frontend/pages/completedworkshops.tsx
@@ -6,24 +6,13 @@ import Link from 'next/link';
 import bioTrainLogo from '../public/biotrainlogo.png';
 
 const CompletedWorkshopsPage = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  
-  const handleYesClick = () => {
-    setShowDropdown(true); // Show the dropdown
-  };
-
-  
-  const handleNoClick = () => {
-    setShowDropdown(false); // Hide the dropdown
-  };
 
   return (
     <div className="container">
       <Image src={bioTrainLogo} alt="Bio train Logo" width={339} height={141} />
       <h1 className="header">Have you completed any workshops previously?</h1>
 
-      <div>
+      <div className="button-container">
         <Link href="/goals">
           <button className="btn">No</button>
         </Link>
